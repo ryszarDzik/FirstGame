8c4716b542ae06f69a40e5df2c67fa5470e2a3cb
Ryszard Miodoński <rmiodonski@biuro.insignistfi.pl>
2025-07-01 23:38:07 +0200

    Dodane intro tekstowe do gry

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Welcome to the First Game!")
+name = input("What is your name? ")
+age = int(input("How old are you? ").strip())
+print(f"Hello {name}, you are {age} years old!")
+
+if age >= 18:
+    print("You are old enough to play this game!")
+    wants_to_play = input("Do you want to play? (yes/no) ").strip().lower()
+    if wants_to_play == 'yes':
+        print("Great! Let's start the game.")
+else:
+    print("You are not old enough to play this game!")
+  
